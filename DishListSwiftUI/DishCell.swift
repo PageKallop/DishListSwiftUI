ef667a6d6def66a35b9a648c95b956e6ad0e56d6
PageKallop <PageMarie78@gmail.com>
2021-01-20 12:52:24 -0600

    Set up phone size preview Set up dark mode preview

--- DishListSwiftUI/DishCell.swift
+++ DishListSwiftUI/DishCell.swift
@@ -11,22 +11,34 @@ import  SwiftUI
 struct DishCell: View {
     
     var body: some View {
+        
+        NavigationView {
         HStack{
             Image("1dish")
                 .resizable()
                 .frame(width: 100, height: 100)
+                .clipShape(Circle())
         VStack(alignment: .leading) {
         Text("Filet Mignon")
         Text("$35")
         }
-      }
+        } .padding(5)
     }
+ }
 }
 
 
 struct DishCell_Preview: PreviewProvider {
     static var previews: some View {
-        DishCell()
+        
+        Group {
+            DishCell().colorScheme(.dark)
+            
+            DishCell().previewDevice("iPhone SE")
+            
+            DishCell().environment(\.sizeCategory, .extraSmall)
+            DishCell().environment(\.sizeCategory, .extraLarge)
+        }
     }
     
 }
